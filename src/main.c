8d5d9271330f8566a0d0adf2bd23d4c8fca64a61
Michael Schmitt <leahcim.s@t-online.de>
2025-12-04 15:53:01 +0100

    implemented small demo

--- src/main.c
+++ src/main.c
@@ -1,20 +1,73 @@
-#include <stdio.h>
 #include "raylib.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-int main() {
-  printf("Hello to my beautifull programm\n");
+const int screenWidth = 800;
+const int screenHeight = 600;
+const int targetFPS = 60;
 
+void movePos(float dx, float dy, Vector2 *pos) {
+  float newX = pos->x + dx;
+  float newY = pos->y + dy;
 
-  InitWindow(800, 600, "Raylib + Meson");
-    SetTargetFPS(60);
+  if (newX < 0) newX = 0;
+  if (newX > screenWidth) newX = screenWidth;
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello from Raylib + Meson!", 200, 300, 30, DARKGRAY);
-        EndDrawing();
-    }
+  if (newY < 0) newY = 0;
+  if (newY > screenHeight) newY = screenHeight;
 
-    CloseWindow();
-    return 0;
+  pos->x = newX;
+  pos->y = newY;
+}
+
+Color getRGBColor(float t) {
+  return (Color){127.5f + 127.5f * sinf(t), 127.5f + 127.5f * sinf(t + 2.0944f),
+                 127.5f + 127.5f * sinf(t + 4.1888f), 255};
+}
+
+int main(void) {
+  InitWindow(screenWidth, screenHeight, "Raylib Window");
+  SetTargetFPS(targetFPS);
+
+  Vector2 playerPos = {(float)screenWidth / 2, (float)screenHeight / 2};
+
+  int speed = 100;
+  Vector2 funnyBallPos = {0, 0};
+  Vector2 funnyBallSpeed = {speed * 5, speed * 5};
+  float t = 0;
+
+  while (!WindowShouldClose()) {
+    float dt = GetFrameTime();
+
+    if (IsKeyDown(KEY_W)) movePos(0, -speed * dt, &playerPos);
+    if (IsKeyDown(KEY_A)) movePos(-speed * dt, 0, &playerPos);
+    if (IsKeyDown(KEY_S)) movePos(0, speed * dt, &playerPos);
+    if (IsKeyDown(KEY_D)) movePos(speed * dt, 0, &playerPos);
+
+    if (IsKeyPressed(KEY_Q)) break;
+
+    if (funnyBallPos.x > screenWidth || funnyBallPos.x < 0)
+      funnyBallSpeed.x *= -1;
+    if (funnyBallPos.y > screenHeight || funnyBallPos.y < 0)
+      funnyBallSpeed.y *= -1;
+
+    funnyBallPos.x += funnyBallSpeed.x * dt;
+    funnyBallPos.y += funnyBallSpeed.y * dt;
+
+    t += 3 * dt;
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    DrawText("Super tolle Demo !!!", 250, 300, 30, DARKGRAY);
+
+    DrawCircleV(playerPos, 10, BLUE);
+    DrawCircleV(funnyBallPos, 20, getRGBColor(t));
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
